55adcbd6d26ca8dc2f9ecc0adc2d17042fbb8d0f
Looser-yashh <63383429+Looser-yashh@users.noreply.github.com>
2021-04-14 12:08:25 +0530

    Create binarySearchForFirstOccurrence.cpp

---
+++ Binary search/binarySearchForFirstOccurrence.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int main(){
+	vector <ll> v{1,3,5,5,5,6,7,67,124,124,124};
+	ll l = 0;
+	ll r = (ll)v.size()-1;
+	ll key = 4;
+	ll ans = -1;
+	while(l<=r){
+		ll mid = (l+r)/2;
+		if(key>v[mid]){
+			l = mid+1;
+		}
+		else{
+			ans = mid;
+			r = mid-1;
+		}
+	}
+	cout << ans << endl;
+	return 0;
+}
