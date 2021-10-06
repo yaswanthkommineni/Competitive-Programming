6d82a53bf9496d9bae3bf7a7f11f3ae0ba00cb81
yaswanth phani kommineni <63383429+yaswanthPhaniKommineni@users.noreply.github.com>
2021-10-06 10:15:03 +0530

    Update BIT.cpp

--- Binray indexed tree/BIT.cpp
+++ Binray indexed tree/BIT.cpp
@@ -1,63 +1,38 @@
-/*
- * yaswanth phani kommineni
- */
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-#define endl '\n';
-
-// use this part
-struct FenwickTree {
-	vector<ll> bit;  // binary indexed tree
-	ll n;
-	FenwickTree(ll s) {
-		n = s;
-		bit.assign(n, 0);
-	}
-	FenwickTree(vector<ll> a) : FenwickTree(a.size()) {
-		for (ll i = 0; i < (ll)a.size(); i++)
-		add(i, a[i]);
-	}
-	int sum(ll r) {
-		ll ret = 0;
-		for (; r >= 0; r = (r & (r + 1)) - 1)
-		ret += bit[r];
-		return ret;
-	}
-	int sum(ll l, ll r) {
-		return sum(r)-sum(l-1);
-	}
-	void add(ll idx, ll delta) {
-		for (; idx < n; idx = idx | (idx + 1))
-		bit[idx] += delta;
-	}
+// start
+template <class T> struct FenwickTree {
+        vector<T> fwt;
+        int n;
+ 
+        FenwickTree(int n_) {
+                n = n_;
+                fwt.assign(n, 0);
+        }
+ 
+        FenwickTree(vector<T>& a) {
+                n = (int)a.size();
+                fwt.assign(n, 0);
+                for (int i = 0; i < (int)a.size(); i++) {
+                        add(i, a[i]);
+                }
+        }
+ 
+        T sum(int r) {
+                T ret = 0;
+                for (; r >= 0; r = (r & (r + 1)) - 1) 
+                        ret += fwt[r];
+                return ret;
+        }
+ 
+        T query(int l, int r) {
+                if (l > r) {
+                        return 0;
+                }
+                return sum(r) - sum(l - 1);
+        }
+        
+        void add(int idx, T delta) {
+                for (; idx < n; idx = idx | (idx + 1)) 
+                        fwt[idx] += delta;
+        }
 };
-// usable part ended
-// --------------------------------------------------------------------------
-
-void solve(){
-	ll n;
-	cin >> n;
-	vector <ll> v(n);
-	for(ll i = 0;i<n;i++) cin >> v[i];
-	struct FenwickTree bit(v);
-	for(ll i=0;i<n;i++){
-		cout << bit.sum(i) << endl;
-	}
-}
-
-int main(){
-	freopen("input.txt","r",stdin);
-	//freopen("output.txt","w",stdout);
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
-	int tc;
-	tc = 1;
-	//cin >> tc;
-	for(int i=1;i<=tc;i++){
-		solve();
-	}
-	return 0;
-}
-
+// end
